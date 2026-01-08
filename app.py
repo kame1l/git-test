cdbfec2819db094c98953f40e37a35a51c1930aa
kame1l <506578@student.firda.nl>
2026-01-08 18:26:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ x = 10
 y = 20
 print("The sum is:", x + y)
 print("Update from GitHub")
+
+Commit directly to the main branch
+
+Create a new branch for this commit and start a pull request Learn more about pull requests
